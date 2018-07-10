485b77361cf060da90d641aa9ac7b1ff6bcfcfa3
Guilherme Bialas <38558633+GuilhermeBialas@users.noreply.github.com>
2018-07-10 11:12:15 -0300

    Add files via upload

---
+++ Exercicio07ListaCidadesSQL02.sql
@@ -0,0 +1,2 @@
+#7. Contabilize a quantidade de cidades do estado de SC
+SELECT count(estado) FROM cidades WHERE estado LIKE 'SC';
---
+++ Exercicio8ListaCiadesSQL02.sql
@@ -0,0 +1,2 @@
+#8. Contabilize a quantidade de cidades do estado de SP.
+SELECT count(estado) from cidades WHERE estado LIKE'SP';
